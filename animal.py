1b036bb0c2a427225c67ef0168441d73e3530388
hgoodroe <102561661+hgoodroe@users.noreply.github.com>
2022-06-15 22:08:43 -0500

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Matthew Hunter Goodroe
+# Github username: hgoodroe
+# Date: 3/30/2022
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
